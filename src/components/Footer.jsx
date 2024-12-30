83c1d37410e5279a7fe3bf977b01e2198aa0d963
downzer0 <cr3r8uyk@gmail.com>
2024-12-30 09:49:52 -0500

    feat: site infrastructure prep

---
+++ src/components/Footer.jsx
@@ -0,0 +1,77 @@
+import { FacebookIcon } from "../assets/icons/FacebookIcon";
+import { InstagramIcon } from "../assets/icons/InstagramIcon";
+import { TailcastLogo } from "../assets/logos/TailcastLogo";
+import { TwitterIcon } from "../assets/icons/TwitterIcon";
+
+const footerData = [
+  {
+    title: "Important Links",
+    items: ["Terms & Conditions", "Privacy Policy"],
+  },
+];
+
+export const Footer = () => {
+  return (
+    <footer aria-label="Site footer">
+      <div className="pt-10  lg:pt-20 lg:pb-16 bg-bgDark1 radius-for-skewed ">
+        <div className="container mx-auto px-4 w-4/5 md:w-11/12 lg:w-10/12 xl:w-4/5 2xl:w-2/3">
+          <div className="flex flex-wrap">
+            <div className="w-full lg:w-1/3 mb-16 lg:mb-0">
+              <div className="flex justify-center lg:justify-start items-center grow basis-0">
+                <div className="text-white mr-2 text-6xl">
+                  <TailcastLogo />
+                </div>
+                <div className="text-white font-['Inter'] font-bold text-xl">
+                  Dead City
+                </div>
+              </div>
+              <div className="w-36 mx-auto lg:mx-0">
+                <a
+                  className="inline-block w-10  h-10 mr-2 p-2 pt-[0.55rem] outlined-button"
+                  href="#"
+                  aria-label="Facebook"
+                >
+                  <FacebookIcon />
+                </a>
+                <a
+                  className="inline-block w-10  h-10 mr-2 p-2 pt-[0.55rem] pl-[0.55rem] outlined-button"
+                  href="#"
+                  aria-label="Twitter"
+                >
+                  <TwitterIcon />
+                </a>
+                <a
+                  className="inline-block w-10  h-10 mr-2 p-2 pt-[0.55rem] pl-[0.55rem] outlined-button"
+                  href="#"
+                  aria-label="Instagram"
+                >
+                  <InstagramIcon />
+                </a>
+              </div>
+            </div>
+            <div className="w-full lg:w-2/3  lg:pl-16 hidden lg:flex flex-wrap justify-between">
+              <div className="w-full md:w-1/3 lg:w-auto mb-16 md:mb-0">
+                <h3 className="mb-6 text-2xl font-bold text-primaryText">
+                  Important Links
+                </h3>
+                <ul>
+                  {footerData[0].items.map((item, index) => (
+                    <li key={`${item}-${index}`} className="mb-4">
+                      <a
+                        className="text-gray-400 hover:text-gray-300"
+                        href="#"
+                        aria-label={item}
+                      >
+                        {item}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
